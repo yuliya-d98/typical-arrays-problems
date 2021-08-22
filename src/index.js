b3e8b1ea10fa1b316425e680fe27ab323fd3f928
yuliya-d98 <83913169+yuliya-d98@users.noreply.github.com>
2021-08-22 23:23:35 +0300

    add: task solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 
 exports.min = function min (array) {
-  return 0;
+  let result;
+  if (array == undefined) {
+      result = 0;
+  } else if (array.length == 0) {
+      result = 0;
+  } else {
+      result = array.sort((a, b) => a - b)[0];
+  }
+  return result;
 }
 
 exports.max = function max (array) {
-  return 0;
+  let result;
+  if (array == undefined) {
+      result = 0;
+  } else if (array.length == 0) {
+      result = 0;
+  } else {
+      result = array.sort((a, b) => a - b)[array.length - 1];
+  }
+  return result;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let result;
+  if (array == undefined) {
+      result = 0;
+  } else if (array.length == 0) {
+      result = 0;
+  } else {
+      result = (array.reduce((a,b) => a + b) / array.length);
+  }
+  return result;
 }
